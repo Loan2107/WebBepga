b0fbfd33be604a5348a23c2dd554e3532753ea30
Jang <101407476+Zentnon@users.noreply.github.com>
2022-10-04 16:34:55 +0700

    Update detailView_fixed.php

--- modules/products/views/detailView_fixed.php
+++ modules/products/views/detailView_fixed.php
@@ -27,7 +27,7 @@
                         <div class="desc">
                             <p>Loại bếp :<?php echo $data['1']['type_of_gas_stove']; ?></p>
                             <p>Tiện ích :<?php echo $data['1']['utilities']; ?></p>
-                            <p>Hãng :<?php echo $data['1']['brand']; ?></p>
+                            <p>Năm sản xuất :<?php echo $data['1']['year_of_manufacture']; ?></p>
                             <p>Sản xuất tại :<?php echo $data['1']['manufacturer_in']; ?></p>
                             <p>Gas tiêu thụ :<?php echo $data['1']['gas_consumption'] ;?></p>
                             <p>Đánh lửa :<?php echo $data['1']['igniters']; ?></p>
